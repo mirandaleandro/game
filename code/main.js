7cf0c144a560bcabe0be1b83cc7cbef061fe33a2
mirandaleandro <mirandaleandro@gmail.com>
2015-02-14 04:50:28 -0700

    removing empty never used gmae controller.

--- code/main.js
+++ code/main.js
@@ -1,11 +1,9 @@
 //Main code that holds the inital steps of the game logic
 
 var memoryController = require('memoryController');    
-var gameController = require('gameController');
 var creepController = require('creepController');
 var spawnerController = require('spawnerController');
 
 memoryController.init();
-gameController.init();
 spawnerController.init();
 creepController.init();
